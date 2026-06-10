eac0dd3431e172ecf374dfc8fa078f9808c916c6
PrincySingh04 <princys549@gmail.com>
2026-06-10 08:29:02 +0530

    initial commit

---
+++ initial/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function App(){
+  const name='princy'
+  const age = '22'
+  return(
+    <div>
+      <h2>my name is {name}</h2>
+      <h3>i am {age} year's old</h3>
+    </div>
+  )
+}
+export default App
